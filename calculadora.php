2b21136b5af17906e726fd45a7d44e186d5d5781
Alex <asl_opc@hotmail.com>
2015-03-31 21:09:26 -0300

    Criando segundo arquivo

---
+++ calculadora.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<form method="post" action="calculadora.php">
+<input type="text" name="numero1"/>
+<input type="text" name="numero2"/>
+<INPUT TYPE="submit" VALUE="Enviar">
+</form>
+<?php
+if(isset($_POST['numero1']))
+{
+$soma = $_POST['numero1'] + $_POST['numero2'];
+
+echo "O Resultado da soma será ". $soma;
+
+}
+
+?>
+
+<body>
+
+</html> 
+
